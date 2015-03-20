51cf02245603ddc663f1556741e49a8537719d66
Samrith Shankar <samrith.298@gmail.com>
2015-03-20 15:48:55 +0530

    Initial console testing

--- animania.js
+++ animania.js
@@ -1,5 +1,10 @@
 /*----------------------------------
-* ANIMANIA Custom Animation Plugin
+*       ___   __   _   _       ___  ___       ___   __   _   _       ___  
+*      /   | |  \ | | | |     /   |/   |     /   | |  \ | | | |     /   | 
+*     / /| | |   \| | | |    / /|   /| |    / /| | |   \| | | |    / /| | 
+*    / /_| | | |\   | | |   / / |__/ | |   / /_| | | |\   | | |   / /_| | 
+*   / ___  | | | \  | | |  / /       | |  / ___  | | | \  | | |  / ___  | 
+*  /_/   |_| |_|  \_| |_| /_/        |_| /_/   |_| |_|  \_| |_| /_/   |_| 
 *
 * Author: Samrith Shankar
 * Version: 1.0.0
@@ -16,21 +21,66 @@
 ----------------------------------*/
 
 $.fn.animania = function(value, options, callback) {
-    
+    if(typeof(value)!="undefined") {
+        if(!value.isString) options = value;
+        if(options.length<1) callback = options;
+    }
+    else
+        value="float";
+
     var settings = $.extend({
         duration: 400,
         direction: "down",
-        
-        
-    });
-    switch(value) {
-            case "float":
-                floatanim(options, callback);
-                break;
-            
-            case "
-    }
+    }, options);
     
     var floatanim = function(options, callback) {
+        console.log("Floatanim called");
+        console.log(options.duration);
+    };
+    
+    var puffanim = function(options, callback) {
+        console.log("Puffanim called");
+        console.log(options.duration);
+    }
+    
+    var moveanim = function(options, callback) {
+        console.log("Moveanim called");
+        console.log(options.duration);
+    }
+    
+    var fadeanim = function(options, callback) {
+        console.log("Fadeanim called");
+        console.log(options.duration);
+    }
+    
+    var creditsanim = function(options, callback){
+        console.log("Creditsanim called");
+        console.log(options.duration);
+    }
+    
+    switch(value) {
+            case "float":
+                floatanim(settings, callback);
+                break;
+            
+            case "puff":
+                puffanim(settings, callback);
+                break;
+            
+            case "move":
+                moveanim(settings,callback);
+                break;
+            
+            case "fade":
+                fadeanim(settings,callback);
+                break;
+            
+            case "credits":
+                creditsanim(settings,callback);
+                break;
+            
+            default:
+                floatanim(settings,callback);
+                break;
     }
 };
